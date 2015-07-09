afd5488b31cd4663f370c61b2ccefc1608f926cb
Lucas Liendo <mindmaster@gmail.com>
2015-07-09 16:08:32 -0300

    - Added self.units attribute that holds common measurement units.  - Added help messages for all options.  - All platform queries now update mem_stats dicttionary with : 'in use', 'total' and 'free' key/values.  - Added _get_thresholds() method.  - Implemented _get_current_status() and _get_detailed_output() methods.

--- checks/ram-usage.py
+++ checks/ram-usage.py
@@ -22,7 +22,7 @@ Copyright 2015 Lucas Liendo.
 """
 
 
-from math import ceil
+from math import floor
 from platform import system as platform_name
 from json import dumps as serialize_json
 from argparse import ArgumentParser
@@ -43,15 +43,30 @@ class RamUsage(object):
             'Windows': self._windows_ram_usage,
         }
 
+        self.units = {
+            'bytes': 1,
+            'kib': 1024,
+            'mib': 1024 ** 2,
+            'gib': 1024 ** 3,
+            'per': 1,
+        }
+
         self._cli_options = self._build_argument_parser().parse_args()
 
     def _build_argument_parser(self):
         parser = ArgumentParser(prog=self.PROGRAM_NAME)
-        parser.add_argument('-p', '--partition', dest='partition', action='store', required=True)
-        parser.add_argument('-u', '--units', dest='units', action='store')
-        parser.add_argument('-O', '--ok', dest='ok_threshold', action='store', required=True)
-        parser.add_argument('-W', '--warning', dest='warning_threshold', action='store', required=True)
-        parser.add_argument('-S', '--severe', dest='severe_threshold', action='store', required=True)
+        parser.add_argument(
+            '-u', '--units', dest='units', action='store', default='mib',
+            help='Valid units are : kib, mib, gib or per. Default is mib.'
+        )
+        parser.add_argument(
+            '-O', '--ok', dest='ok_threshold', action='store', required=True,
+            help='Ok status range. E.g : (0,1500]. Don\'t include brackets.'
+        )
+        parser.add_argument(
+            '-W', '--warning', dest='warning_threshold', action='store', required=True,
+            help='Warning status range. E.g : (1500,1700]. Don\'t include brackets.'
+        )
         parser.add_argument('-v', '--version', action='version', version=self.PROGRAM_VERSION)
 
         return parser
@@ -69,11 +84,17 @@ class RamUsage(object):
 
         try:
             with open('/proc/meminfo') as fd:
-                mem_info = {k: v for k, v in [_process_meminfo_line(l) for l in fd.readlines()]}
+                mem_stats = {k: v for k, v in [_process_meminfo_line(l) for l in fd.readlines()]}
         except IOError, e:
             raise RamUsageError('Error - Couldn\'t open /proc/meminfo. Details : {:}'.format(e))
 
-        return mem_info
+        mem_stats.update({
+            'in use': mem_stats['MemTotal'] - mem_stats['MemFree'],
+            'free': mem_stats['MemFree'],
+            'total': mem_stats['MemTotal'],
+        })
+
+        return mem_stats
 
     def _windows_ram_usage(self):
         try:
@@ -81,34 +102,69 @@ class RamUsage(object):
         except ImportError:
             raise RamUsageError('Error - Couldn\'t import win32api.')
 
-        mem_info = GlobalMemoryStatusEx()
-        mem_info.update({
-            'MemFree': mem_info['TotalPhys'] - ceil((mem_info['MemoryLoad'] / 100.0) * mem_info['TotalPhys'])
+        mem_stats = GlobalMemoryStatusEx()
+        in_use = floor((mem_stats['MemoryLoad'] / 100.0) * mem_stats['TotalPhys'])
+
+        mem_stats.update({
+            'in use': in_use,
+            'free': mem_stats['TotalPhys'] - in_use,
+            'total': mem_stats['TotalPhys'],
         })
 
-        return mem_info
+        return mem_stats
 
-    def _get_current_status(self, seconds):
-        pass
+    def _get_thresholds(self):
+        try:
+            units = self.units[self._cli_options.units]
+            min_ok, max_ok, min_warning, max_warning = [
+                float(t) * units for t in self._cli_options.ok_threshold.split(',') + self._cli_options.warning_threshold.split(',')
+            ]
 
-    def _get_detailed_output(self, seconds):
-        pass
+            return {
+                'min ok': min_ok,
+                'max ok': max_ok,
+                'min warning': min_warning,
+                'max warning': max_warning,
+            }
+        except ValueError:
+            raise RamUsageError('')
+        except KeyError:
+            raise RamUsageError('Error - Wrong \'{:}\' units parameter.')
+
+    def _get_current_status(self, mem_stats):
+        status = 'SEVERE'
+        thresholds = self._get_thresholds()
+
+        if thresholds['min ok'] < mem_stats['in use'] <= thresholds['max ok']:
+            status = 'OK'
+        elif thresholds['min warning'] < mem_stats['in use'] <= thresholds['max warning']:
+            status = 'WARNING'
+
+        return status
+
+    def _get_detailed_output(self, mem_stats):
+        return 'total : {:.2f}, in use : {:.2f}, free : {:.2f}.'.format(
+            *[mem_stats[k] / float(self.units[self._cli_options.units]) for k in ['total', 'in use', 'free']]
+        )
 
     def get(self):
         output = {'status': 'ERROR'}
         platform = platform_name()
 
         try:
-            mem_info = int(self.available_platforms[platform]())
+            mem_stats = self.available_platforms[platform]()
             output.update({
-                'status': self._get_current_status(mem_info),
-                'details': self._get_detailed_output(mem_info),
-                'data': {'name': self.PROGRAM_NAME, 'uptime': mem_info},
+                'status': self._get_current_status(mem_stats),
+                'details': self._get_detailed_output(mem_stats),
+                'data': {
+                    'name': self.PROGRAM_NAME,
+                    'in use': mem_stats['in use'],
+                    'free': mem_stats['free'],
+                    'total': mem_stats['total'],
+                },
             })
         except RamUsageError, e:
             output.update({'details': str(e)})
-        except ValueError:
-            output.update({'details': 'Error - uptime\'s \'-s\' argument must be a valid interger value.'})
         except KeyError:
             output.update({'details': '{:} platform is not supported.'.format(platform)})
 
